b2507e167e57c5e748cf012eaf5c8de6e488a556
Jonathan Hoyt <hoyt@github.com>
2015-12-17 16:41:10 -0800

    Pretty solid reader code

--- src/brain.ino
+++ src/brain.ino
@@ -1,215 +1,72 @@
 #define rxPin 4
-#define txPin 5
+#define offState 1
+#define onState 0
 
 int val = 0;
-int start = 0;
-int start2 = 0;
-int end = 0;
-int end2 = 0;
-int loopCount = 0;
-int in_message = 0;
-
+double bit_start_timestamp = 0;
+double bit_end_timestamp = 0;
+double message_start_timestamp = 0;
 int bit_counter = 0;
-int message_start = 0;
-int message_end = 0;
+double last_bit_timestamp = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  digitalWrite(txPin, LOW);
 }
 
-void write_one() {
-  delayMicroseconds(10);
-  digitalWrite(txPin, HIGH);
-  delayMicroseconds(400);
-  digitalWrite(txPin, LOW);
-  delayMicroseconds(90);
+boolean is_bit_start() {
+  return val == onState && bit_start_timestamp == 0;
 }
 
-void write_zero() {
-  delayMicroseconds(10);
-  digitalWrite(txPin, HIGH);
-  delayMicroseconds(100);
-  digitalWrite(txPin, LOW);
-  delayMicroseconds(390);
+boolean is_bit_end() {
+  return val == offState && bit_start_timestamp !=0;
 }
 
-void write_nothing() {
-  delay(20);
-  Serial.println("");
+boolean is_bit_complete() {
+  return val == offState && bit_start_timestamp != 0 && bit_end_timestamp != 0;
 }
 
-void send_message(char *message) {
-  Serial.println("start of message");
-  for(unsigned int i = 0; i < strlen(message); i++) {
-    char bit = message[i];
-    if(bit == '0') {
-      write_zero();
-    } else if(bit == '1') {
-      write_one();
-    } else if(bit == '|') {
-      write_nothing();
-    }
-  }
-  Serial.println("\nend of message");
+boolean is_zero() {
+  return (bit_end_timestamp - bit_start_timestamp) < 200;
 }
 
-void listen(double delay_microseconds) {
-  unsigned int t = micros();
-  unsigned int loopCount = 0;
-  unsigned int start = 0;
-  unsigned int end = 0;
+boolean is_one() {
+  return (bit_end_timestamp - bit_start_timestamp) > 200;
+}
 
-  Serial.print("listening for ");
-  Serial.println(delay_microseconds);
-
-  while(micros() < t + delay_microseconds) {
-    int val = digitalRead(rxPin);
-    loopCount++;
-
-    if(val == 0 && start == 0) {
-      start = micros();
-    } else if (val == 1 && start !=0) {
-      end = micros();
-    }
-
-    in_message = loopCount < 100;
-
-    if (start != 0 && end != 0) {
-      if (in_message) {
-        if ((end - start) < 200) {
-          Serial.print(0);
-        } else {
-          Serial.print(1);
-        }
-      } else {
-        Serial.println("");
-      }
-
-      loopCount = 0;
-      start = 0;
-      end = 0;
-    }
-  }
-
-  Serial.println("done listening");
+boolean is_message_boundary() {
+  return last_bit_timestamp != 0 && (micros() - last_bit_timestamp) > 5000;
 }
 
 void loop() {
-  // ************************** WRITE
-  // delay(2000);
-  // listen(100000);
-  // char message1[] = "1000011010011|100001101001000000100|";
-  // send_message(message1);
-  // listen(29000000);
-  // // delayMicroseconds(29000); // 58 characters * 500 micros for 3rd radio message
-  // char message2[] = "|10000110100100010";
-  // send_message(message2);
-  // delay(2000000);
-
-  // ************************** READ
   val = digitalRead(rxPin);
-  loopCount++;
 
-  if(val == 0 && start == 0) {
-    start = micros();
-  } else if (val == 1 && start !=0) {
-    end = micros();
+  if(is_bit_start()) {
+    if (is_message_boundary()) {
+      Serial.print(" ");
+      Serial.print((last_bit_timestamp - message_start_timestamp) / bit_counter);
+
+      bit_counter = 0;
+      message_start_timestamp = micros();
+
+      Serial.println();
+      last_bit_timestamp = 0;
+    }
+    bit_start_timestamp = micros();
+  } else if (is_bit_end()) {
+    bit_end_timestamp = micros();
   }
 
-  in_message = loopCount < 1000;
-
-  if (start != 0 && end != 0) {
-    if (in_message) {
-      if ((end - start) < 200) {
-        Serial.print(0);
-      } else {
-        Serial.print(1);
-      }
-    } else {
-      Serial.println("");
+  if (is_bit_complete()) {
+    if (is_zero()) {
+      Serial.print(0);
+    } else if (is_one()) {
+      Serial.print(1);
     }
 
-    loopCount = 0;
-    start = 0;
-    end = 0;
+    bit_start_timestamp = 0;
+    bit_end_timestamp = 0;
+    last_bit_timestamp = micros();
+    bit_counter++;
   }
-
-  // ************ trying to calculate distance between bits
-  // val = digitalRead(rxPin);
-  //
-  // if(val == 0 && start == 0) {
-  //   start = micros();
-  // } else if (val == 1 && start !=0) {
-  //   end = micros();
-  // }
-  //
-  // if(val == 1 && start2 == 0) {
-  //   start2 = micros();
-  // } else if (val == 0 && start2 !=0) {
-  //   end2 = micros();
-  // }
-  //
-  // if(start != 0 && end != 0 && start2 != 0 && end2 != 0) {
-  //   int up = end - start;
-  //   int down = end2 - start2;
-  //   Serial.print("Up: ");
-  //   Serial.println(up);
-  //   Serial.print("Down: ");
-  //   Serial.println(down);
-  //   Serial.print("Total: ");
-  //   Serial.println(up + down);
-  //
-  //   start2 = 0;
-  //   end2 = 0;
-  //   start = 0;
-  //   end = 0;
-  // }
-
-  // Divide total message time by number of bits to get micros per bit
-  //
-  // val = digitalRead(rxPin);
-  // loopCount++;
-  //
-  // if(val == 0 && start == 0) {
-  //   start = micros();
-  // } else if (val == 1 && start !=0) {
-  //   end = micros();
-  // }
-  //
-  // in_message = loopCount < 100;
-  //
-  // if (start != 0 && end != 0) {
-  //   if (in_message) {
-  //     if ((end - start) < 200) {
-  //       // Serial.print(0);
-  //     } else {
-  //       // Serial.print(1);
-  //     }
-  //     bit_counter++;
-  //     message_end = micros();
-  //   } else {
-  //     Serial.println("");
-  //
-  //     if(message_start == 0) {
-  //       message_start = micros();
-  //     } else {
-  //       Serial.print("microseconds per bit: ");
-  //       Serial.println((message_end - message_start) / bit_counter);
-  //       // Serial.print("total: ");
-  //       // Serial.println(message_end - message_start);
-  //       // Serial.print("bit counter: ");
-  //       // Serial.println(bit_counter);
-  //       bit_counter = 0;
-  //       message_start = 0;
-  //       message_end = 0;
-  //     }
-  //   }
-  //
-  //   loopCount = 0;
-  //   start = 0;
-  //   end = 0;
-  // }
 }
