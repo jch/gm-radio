db6787d2e88715b94394dfa43033b2da0e45194c
Jonathan Hoyt <hoyt@github.com>
2016-01-14 17:02:02 -0800

    The start of a long conversation

--- src/brain.ino
+++ src/brain.ino
@@ -120,36 +120,132 @@ void setup() {
   pinMode(txPin, OUTPUT);
   digitalWrite(txPin, LOW);
 
-  // delay(4000);
-  //
-  // char cd_changer_hello[] = "1100001101001000000100";
-  // String head_response1 = "111011110100110010";
-  // String head_response2 = "111001110100110011";
-  // String ideal_head_response = "111001101110000100";
-  // char yeah[] = "110000110100000011";
-  // boolean unmatched = true;
-  //
-  // send_message(cd_changer_hello);
-  //
-  // while(unmatched) {
-  //   response = listen_for_message();
-  //   Serial.println(response);
-  //
-  //   if(response == head_response1) {
-  //     unmatched = false;
-  //     send_message(cd_changer_hello);
-  //   } else if(response == head_response2) {
-  //     unmatched = false;
-  //     send_message(cd_changer_hello);
-  //   } else if(response == ideal_head_response) {
-  //     unmatched = false;
-  //     send_message(yeah);
-  //   }
-  // }
+  boolean unmatched = true;
+
+  delay(4000);
+
+  // listen for head1 until we hear it
+  char head1[] = "1101010100";
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head1) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // listen for head2 until we hear it
+  char head2[] = "111001110100110011";
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head2) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // listen for head3 until we hear it
+  char head3[] = "111001101111";
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head3) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // listen for head4 until we hear it
+  char head4[] = "1100001011111111";
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head4) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // listen for head5 until we hear it
+  char head5[] = "111011110100110010";
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head5) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // wait 4 milliseconds and then respond with hello
+  delay(4);
+  char cd_changer_hello[] = "1100001101001000000100";
+  send_message(cd_changer_hello);
+
+  // listen for head2 until we hear it
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head2) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // wait 7 milliseconds and then respond with hello
+  delay(7);
+  send_message(cd_changer_hello);
+
+  // listen for head6 until we hear it
+  char head6[] = "111001110100010010";
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head6) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // say hello again because why not
+  delay(7);
+  send_message(cd_changer_hello);
+
+  // wait a bit and do this
+  delay(50);
+  char cd_changer_sup[] = "110000110100100010";
+  send_message(cd_changer_sup);
+
+  // listen for head3 until we hear it
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head3) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // listen for head6 until we hear it
+  while(unmatched) {
+    response = listen_for_message();
+
+    if(response == head6) {
+      unmatched = false;
+    }
+  }
+  unmatched = true;
+
+  // HELLO BIG LONG MESSAGE FROM CD CHANGER
+  delay(50);
+  char cd_changer_sup[] = "11000011010000000000000101";
+  send_message(cd_changer_sup);
+
+  // ... more to come
 }
 
 void loop() {
-  char three[] = "10100011010010010110";
-  send_message(three);
-  delay(1000);
+
 }
